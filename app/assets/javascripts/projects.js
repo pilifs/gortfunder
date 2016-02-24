c1b946e946792b838387b39d7d00e2d9369aaf58
Chris Staikos <cstaikos@gmail.com>
2016-02-24 16:08:01 -0500

    Implement backing via ajax

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('ready page:load', function() {
+  $('.back-project-button').on('click', function() {
+    $.ajax({
+      type: 'POST',
+      url: '/pledges',
+      dataType: 'script',
+      data: 'pledge[reward_id]=' + $(this).attr('data-reward-id')
+    });
+  });
+});
--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,20 @@
 class PledgesController < ApplicationController
+
+  def create
+    @pledge = Pledge.new(pledge_params)
+    @pledge.user = current_user
+
+    if @pledge.save
+      render 'create.js'
+    else
+      render '/projects'
+    end
+
+  end
+
+  private
+
+  def pledge_params
+    params.require(:pledge).permit(:reward_id)
+  end
 end
